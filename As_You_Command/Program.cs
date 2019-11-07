fdbda6b34e4d8ccafb5569e3d745ec8cb100ed64
mligtermoet <54799107+mligtermoet@users.noreply.github.com>
2019-11-07 20:43:44 +0100

    Achievenemt: 'As You Command' bijgewerkt

--- As_You_Command/Program.cs
+++ As_You_Command/Program.cs
@@ -7,14 +7,15 @@ namespace As_You_Command
         static void Main(string[] args)
         {
             Console.Clear();
-
             Console.WriteLine("Hoe groot moet de lijst zijn?");
             int lijstLengte;
             lijstLengte = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
+            
             Console.WriteLine("Tot welk getal mogen de willekeurige getallen gaan?");
             int totGetal;
             totGetal = Convert.ToInt32(Console.ReadLine());
+            
             Console.Clear();
             lijst(lijstLengte, totGetal);
 
@@ -27,48 +28,7 @@ namespace As_You_Command
                     string nummer = RandomNumber.ToString();
                     Console.WriteLine(nummer);
                 }
-                
             }
-
-            //int randomAantal = aantal();
-            //int randomTot = totGetal();
-            //aantal();
-            //totGetal();
-            //int RandomLijst = lijst();
-            //for (int i = 0; i < randomAantal; i++)
-            //{
-
-            //    Console.WriteLine(randomAantal);
-            //}
-
-
-
-            //int lijst()
-            //{
-            //    Random randm = new Random();
-            //    int rnummers = randm.Next(randomTot);
-            //    rnummers.ToString();
-            //    return rnummers;
-            //}
-
-            //int totGetal()
-            //{
-            //    Console.WriteLine("Tot welk getal mogen de willekeurige getallen gaan?");
-            //    int tot;
-            //    tot = Convert.ToInt32(Console.ReadLine());
-            //    Console.Clear();
-            //    return tot;
-            //}
-
-            //int aantal()
-            //{
-            //    Console.WriteLine("Hoe groot moet de lijst zijn?");
-            //    int hoeveel;
-            //    hoeveel = Convert.ToInt32(Console.ReadLine());
-            //    Console.Clear();
-            //    return hoeveel;
-            //}
-
         }
     }
 }
